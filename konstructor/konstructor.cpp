4fd07399947b637a7b88ae0be6f36aedadf198f2
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-17 17:28:30 +0700

    masukan nama mahasiswa

--- konstructor/konstructor.cpp
+++ konstructor/konstructor.cpp
@@ -35,3 +35,17 @@ void mahasiswa::cetak() {
 	cout << endl << "Nim =" << nim << endl;
 	cout << "Nama =" << nama << endl;
 }
+
+int main() {
+	mahasiswa mhs1;
+	mahasiswa mhs2(20);
+	mahasiswa mhs3("Indra");
+	mahasiswa mhs4(30, "Fauzan");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
+}
