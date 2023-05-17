0a33942edf9f557f153d8bff9a3a85c31fb9b465
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-17 16:47:31 +0700

    memasukan data arr destructor

---
+++ destruktor/destruktor.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class angka {
+private:
+	int* arr;
+	int panjang;
+public:
+	angka(int); //Constructor
+	~angka(); //Destructor
+	void cetakData();
+	void isisData();
+};
