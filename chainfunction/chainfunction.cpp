5f65c3131ef7820c8ed69bf821d9ab467f4928f7
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-17 16:43:07 +0700

    int main Judul buku

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -16,3 +16,11 @@ public:
 	}
 
 }bukunya;
+
+int main()
+{
+	// bukunya.setJudul("Matematika);
+	// cout << bukunya.getJudul();
+	cout << bukunya.setJudul("Matematika").getJudul(); //chain function calls
+	return 0;
+}
