358f2e983dbf73fbbcefe9eaab3b8d97034fd025
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-17 17:03:14 +0700

    memasukan isi data

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -27,6 +27,13 @@ angka::~angka() {  //Destructor
 
 void angka::cetakData() {
 	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = " <<arr[i]<<endl;
+	}
+}
+
+void angka::isiData() {
+	for (int i = 1; i <= panjang; i++) {
 		cout << i << " = "; cin >> arr[i];
 	}
+	cout << endl;
 }
