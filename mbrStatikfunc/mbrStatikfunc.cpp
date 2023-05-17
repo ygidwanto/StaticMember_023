ce46b582e4c8f0949ac98a6873af732c1edc9968
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-17 17:46:44 +0700

    mengakses nim melalui static member function "setNim"

--- mbrStatikfunc/mbrStatikfunc.cpp
+++ mbrStatikfunc/mbrStatikfunc.cpp
@@ -22,3 +22,16 @@ void mahasiswa::printAll() {
 	cout << "Nama = " << nama << endl;
 	cout << endl;
 }
+int main() {
+	mahasiswa mhs1("Sri Dadi");
+	mahasiswa mhs2("Budi Jatmiko");
+	mahasiswa::setNim(9); //mengakses nim melalui static member function "setNim"
+	mahasiswa mhs3("Andi Janu");
+	mahasiswa mhs4("Joko Wahono");
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	cout << "akses dari luar object =" << mahasiswa::getNim() << endl; // mengakses nim melaluistatic member function "getNim"
+	return 0;
+			}
