90deeeab82191bbfbc7fa50506db43dd66355cae
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-17 17:36:16 +0700

    masukan statikfunc class mahasiswa

---
+++ mbrStatikfunc/mbrStatikfunc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+	void setID();
+	void printAll();
+	static void setNim(int pNim) { nim = pNim; /*Definisi Function*/ }
+	static int getNim() { return nim; /*Definisi Function*/ }
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
