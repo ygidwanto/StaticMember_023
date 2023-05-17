a5999686937aec1314a2197261044b7a97aa446c
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-17 16:57:23 +0700

    melakukan cetak data

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -24,3 +24,9 @@ angka::~angka() {  //Destructor
 	delete[]arr;
 	cout << " Alamat Array Sudah Dilepaskan" << endl;
 }
+
+void angka::cetakData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = "; cin >> arr[i];
+	}
+}
