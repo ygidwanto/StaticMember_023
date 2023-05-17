1e97ada9a3d47ce96345e22138c08c1f99f92f78
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-17 15:43:00 +0700

    Memasukan Nama mahasiswa dan melakukan print all

--- StaticMember_023/StaticMember_023.cpp
+++ StaticMember_023/StaticMember_023.cpp
@@ -21,3 +21,17 @@ void mahasiswa::printAll() {
 	cout << "NIM = " << nim << endl;
 	cout << endl;
 }
+int main() {
+	mahasiswa mhs1("Lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mhs2.nim = 10;
+	mahasiswa mhs3("Andi Kurniawan");
+	mhs3.nim = 20;
+	mahasiswa mhs4("Joko Purbo");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	return 0;
+}
