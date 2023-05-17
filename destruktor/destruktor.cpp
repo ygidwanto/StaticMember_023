a6301c70a6cce3b2c713c9fa05b73426dbfae47b
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-17 17:06:39 +0700

    Memanggil Destructor

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -37,3 +37,11 @@ void angka::isiData() {
 	}
 	cout << endl;
 }
+
+int main() {
+	angka belajarcpp(3); //Constructor Dipanggil
+	angka* ptrBelajarcpp = new angka(5); //Constructor Dipanggil
+	delete ptrBelajarcpp; //Destructor Dipanggil
+
+	return 0;
+}// Destructor Dipanggil
