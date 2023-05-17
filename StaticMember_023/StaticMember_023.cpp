89934ea23af52814790f62873e33c7bfd14a84dd
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-17 15:23:09 +0700

    Memasukan class Mahasiswa

--- StaticMember_023/StaticMember_023.cpp
+++ StaticMember_023/StaticMember_023.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+	void setId();
+	void printAll();
+	mahasiswa(string pnama) :nama(pnama) { setId(); }
+};
