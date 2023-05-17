68bb3f11d7f97ed174996aa6e21b3df590355bc9
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-17 16:53:29 +0700

    Destructor Delete data

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -11,9 +11,16 @@ public:
 	void cetakData();
 	void isiData();
 };
-//Definisi member function
+//Definisi member functon
 angka::angka(int i) { //Constructor
 	panjang = i;
 	arr = new int[i];
 	isiData();
+}
+
+angka::~angka() {  //Destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << " Alamat Array Sudah Dilepaskan" << endl;
 }
